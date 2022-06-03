80007418af7f23722c997db97fbbaee5115ce7db
Artem <nimoshka@gmail.com>
2022-06-03 22:13:59 +0300

    Complete book cards builder

--- index.js
+++ index.js
@@ -6,56 +6,74 @@ let myLibrary = [
         read: true,
     },
     {
-        title: 'The Hobbit',
-        author: 'J.R.R. Tolkien',
-        pages: 295,
-        read: true,
+        title: 'The Schmobbit',
+        author: 'J.R.R. Schmolkien',
+        pages: 666,
+        read: false,
     }
 ];
 
 document.onload = loopThroughArray();
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+    
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
 // DISPLAY
 
-function loopThroughArray(){
+function loopThroughArray() {
+
     myLibrary.map((elem, index) => {
         buildBookCard(elem, index)
     })
 }
 
 function buildBookCard(obj, index) {
+
     index++;
 
     let card = document.createElement('div');
     let main = document.querySelector('main');
     main.appendChild(card);
-    card.classList.add('book-card', 'book-card-' + index)
+    card.classList.add('book-card', index)
 
     let titleDiv = document.createElement('div');
-    titleDiv.classList.add('title-container', 'title-container-' + index);
+    titleDiv.classList.add('title-container', index);
     card.appendChild(titleDiv);
+    titleDiv.textContent = obj.title;
 
     let authorDiv = document.createElement('div');
-    authorDiv.classList.add('author-container', 'author-container-' + index);
+    authorDiv.classList.add('author-container', index);
     card.appendChild(authorDiv);
+    authorDiv.textContent = obj.author;
 
     let pagesDiv = document.createElement('div');
-    pagesDiv.classList.add('pages-container', 'pages-container-' + index);
+    pagesDiv.classList.add('pages-container', index);
     card.appendChild(pagesDiv);
+    pagesDiv.textContent = obj.pages;
 
     let readDiv = document.createElement('div');
-    readDiv.classList.add('read-container', 'read-container-' + index);
+    readDiv.classList.add('read-container', index);
     card.appendChild(readDiv);
+    printReadStatus(obj.read, readDiv);
+}
+
+function printReadStatus(status, readDiv) {
+
+    if (status) {
+        readDiv.textContent = 'Already read this';
+        readDiv.style.color = 'green';
+    } else {
+        readDiv.textContent = 'Didn\'t read that yet';
+        readDiv.style.color = 'red';
+    }
 }
