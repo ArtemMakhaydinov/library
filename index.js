8fdaff42325ee876790239c13cfac39922c36e7c
Artem <nimoshka@gmail.com>
2022-06-06 17:03:09 +0300

    Split building new book interface on few functions

--- index.js
+++ index.js
@@ -15,16 +15,22 @@ let myLibrary = [
 
 document.onload = loopThroughArray();
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+// CLASS DECLARATION
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
+// ADD
+
 function addBookToLibrary(title, author, pages, read) {
-    
-    let newBook = new Book(title, author, pages, read);
+
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
@@ -32,36 +38,39 @@ function addBookToLibrary(title, author, pages, read) {
 
 function loopThroughArray() {
 
-    myLibrary.map((elem, index) => {
+    myLibrary.forEach((elem, index) => {
         buildBookCard(elem, index)
     })
 }
 
 function buildBookCard(obj, index) {
 
-    index++;
+    const card = document.createElement('div');
+    const main = document.querySelector('main');
+    const readDiv = document.createElement('div');
+    const textFieldNames = ['title', 'author', 'pages'];
 
-    let card = document.createElement('div');
-    let main = document.querySelector('main');
     main.appendChild(card);
     card.classList.add('book-card', index)
 
-    let titleDiv = document.createElement('div');
-    titleDiv.classList.add('title-container', index);
-    card.appendChild(titleDiv);
-    titleDiv.textContent = obj.title;
+    for (let i = 0; i < textFieldNames.length; i++) {
 
-    let authorDiv = document.createElement('div');
-    authorDiv.classList.add('author-container', index);
-    card.appendChild(authorDiv);
-    authorDiv.textContent = obj.author;
+        const fieldDiv = document.createElement('div');
+        const fieldName = document.createElement('div');
+        const fieldContent = document.createElement('div');
 
-    let pagesDiv = document.createElement('div');
-    pagesDiv.classList.add('pages-container', index);
-    card.appendChild(pagesDiv);
-    pagesDiv.textContent = obj.pages;
+        fieldDiv.classList.add(`${textFieldNames[i]}-container`, index);
+        card.appendChild(fieldDiv);
 
-    let readDiv = document.createElement('div');
+        fieldName.classList.add(textFieldNames[i], index);
+        fieldDiv.appendChild(fieldName);
+        fieldName.textContent = textFieldNames[i][0].toUpperCase() + textFieldNames[i].slice(1);
+
+        fieldContent.classList.add(`${textFieldNames[i]}-content`, index);
+        fieldDiv.appendChild(fieldContent);
+        fieldContent.textContent = obj[textFieldNames[i]];
+    }
+
     readDiv.classList.add('read-container', index);
     card.appendChild(readDiv);
     printReadStatus(obj.read, readDiv);
@@ -70,10 +79,137 @@ function buildBookCard(obj, index) {
 function printReadStatus(status, readDiv) {
 
     if (status) {
+
         readDiv.textContent = 'Already read this';
         readDiv.style.color = 'green';
+
     } else {
+
         readDiv.textContent = 'Didn\'t read that yet';
         readDiv.style.color = 'red';
     }
+}
+
+// NEW BOOK BUILD
+
+document.querySelector('.new-book').addEventListener('click', buildNewBookForm);
+
+function buildNewBookForm() {
+
+    const card = document.createElement('div');
+    const form = document.createElement('form');
+
+    animateButtons.call(this);
+    buildEmptyCard(card);
+    buildCardForm(card, form);
+    buildCardButtons(form);
+};
+
+function buildEmptyCard(card) {
+
+    const body = document.querySelector('body');
+    const coverDiv = document.createElement('div');
+
+    coverDiv.classList.add('cover-container');
+    body.appendChild(coverDiv);
+
+    card.classList.add('new-book-card');
+    coverDiv.appendChild(card);
+}
+
+function buildCardForm(card, form) {
+
+    const textFieldNames = ['title', 'author', 'pages', 'read'];
+
+    form.classList.add('new-book-form');
+    card.appendChild(form);
+
+    for (let i = 0; i < textFieldNames.length; i++) {
+        const inputDiv = document.createElement('div');
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+
+        inputDiv.classList.add(`new-book-${textFieldNames[i]}-container`);
+        form.appendChild(inputDiv);
+
+        label.classList.add(`new-book-${textFieldNames[i]}-label`);
+        label.setAttribute('for', `${textFieldNames[i]}`);
+        inputDiv.appendChild(label);
+        label.textContent = textFieldNames[i][0].toUpperCase() + textFieldNames[i].slice(1);
+
+        input.classList.add(`new-book-${textFieldNames[i]}-input`);
+        input.setAttribute('name', `${textFieldNames[i]}`);
+        input.setAttribute('id', `${textFieldNames[i]}`);
+        inputDiv.appendChild(input)
+
+        switch(textFieldNames[i]) {
+            case 'title':
+            case 'author':
+                input.setAttribute('type', 'text');
+                break;
+            case 'pages':
+                input.setAttribute('type', 'number');
+                break;
+            case 'read':
+                input.setAttribute('type', 'checkbox');
+                label.textContent = 'Did you read this book?';
+                break;
+        }
+    }
+}
+
+function buildCardButtons(form) {
+
+    const buttonsDiv = document.createElement('div');
+    const addBookButton = document.createElement('button');
+    const cancelButton = document.createElement('button');
+
+    buttonsDiv.classList.add('new-book-button-container');
+    form.appendChild(buttonsDiv);
+
+    addBookButton.classList.add('new-book-add-button');
+    addBookButton.setAttribute('type', 'submit')
+    buttonsDiv.appendChild(addBookButton);
+    addBookButton.textContent = 'ADD BOOK';
+
+    cancelButton.classList.add('new-book-cancel-button');
+    cancelButton.setAttribute('type', 'button');
+    cancelButton.setAttribute('onclick', 'cancelNewBookForm()')
+    buttonsDiv.appendChild(cancelButton);
+    cancelButton.textContent = 'CANCEL';
+}
+
+// NEW BOOK CANCEL
+
+function cancelNewBookForm() {
+
+    const body = document.querySelector('body');
+    const coverDiv = document.querySelector('.cover-container');
+
+    body.removeChild(coverDiv);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function animateButtons() {
+
+    const button = this;
+
+    button.classList.add('pressed');
+
+    setTimeout(function () {
+        button.classList.remove('pressed')
+    }, 100);
 }
